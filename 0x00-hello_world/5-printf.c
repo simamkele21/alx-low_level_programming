515f65618cbddfce78daefa50c8a64060f200250
Simamkele <91248891+simamkele21@users.noreply.github.com>
2022-09-11 23:43:38 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Begin of execution
  *
@@ -8,6 +7,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
