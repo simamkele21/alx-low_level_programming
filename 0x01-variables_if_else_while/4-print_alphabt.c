d52c670ca82584dec61eb92b0c1de50659444012
simamkele21 <simamkelejanuary@gmail.com>
2022-09-13 15:34:10 +0200

    4.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char c;
+#include"main.h"
 
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-for (c = 'A'; c <= 'Z'; c++)
-putchar(c);
-putchar('\n');
-return (0);
+/**
+ * _isalpha - checks if character is a letter
+ *            both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
+		return (1);
+	return (0);
 }
+
