b8c8a002c80a3b1b31cb5d7af0042bf50c86a2ab
simamkele21 <simamkelejanuary@gmail.com>
2022-09-13 14:54:43 +0200

    .

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,18 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - Check the code for ALX School students
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
- *
- * Return: Always 0 (Success)
- */
+*/
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
