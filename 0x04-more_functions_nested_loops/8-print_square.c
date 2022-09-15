5e8238c53abfc4ce5f8799edd5942a0d6af539db
simamkele21 <simamkelejanuary@gmail.com>
2022-09-15 14:58:17 +0200

    Betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- * print_square - print a square using the character #
- *
- * @size: is the size of the square
- *
- * Return: Always 0 (Success)
+* print_square - print a square using the character #
+*
+* @size: is the size of the square
+*
+* Return: Always 0 (Success)
 */
 
 void print_square(int size)
