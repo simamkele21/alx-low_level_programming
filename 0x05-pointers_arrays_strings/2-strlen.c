fb3ce1c42929059979b5bba94ba1facdecb12b98
simamkele21 <simamkelejanuary@gmail.com>
2022-09-19 12:05:01 +0200

    This report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - The strlen() accepts an argument of type pointer to char or (char*).
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
