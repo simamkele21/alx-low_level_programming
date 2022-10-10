490cef550b9530fec8eed050ffd0e51cde0c2cb6
Simamkele <simamkelejanuary@gmail.com>
2022-10-10 13:52:47 +0200

    .

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,44 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * _strcpy - function that copies the string pointed to
- * @dest: pointer to string entry data
- * @src: destiny data
- * Return: return value of dest
- */
-
-void *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	if (src != '\0')
-	{
-		for (i = 0;  *(src + i) != '\0'; i++)
-		{
-			*(dest + i) = *(src + i);
-		}
-	}
-
-	*(dest + i) = '\0';
-	return (dest);
-}
-/**
- * _strlen - returns the length of a string.
- * @s: pointer to String
- * Return: Nothing
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-/**
  * new_dog - function that creates a new dog
  * @name: name to create a new struct
  * @age: age to create a new struct
